80c8256575f9a3ba75230fe14f2c48270086b61e
ma-za-kpe <makpalyy@gmail.com>
2019-03-27 16:50:07 +0200

    add date property in model file and add component

--- src/app/add-quote/add-quote.component.ts
+++ src/app/add-quote/add-quote.component.ts
@@ -8,7 +8,7 @@ import { Quote } from "../quote";
   styleUrls: ["./add-quote.component.css"]
 })
 export class AddQuoteComponent implements OnInit {
-  newQuote = new Quote(0, "", "", "", 0);
+  newQuote = new Quote(0, "", "", "", 0, new Date());
 
   @Output() addQuote = new EventEmitter<Quote>();
 
