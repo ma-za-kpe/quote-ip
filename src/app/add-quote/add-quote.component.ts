9a1a6632662b29b8a8f90cf8d3a72024ba44eb84
ma-za-kpe <makpalyy@gmail.com>
2019-03-27 14:24:32 +0200

    add new add-quotes component

---
+++ src/app/add-quote/add-quote.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+
+import { Quote } from "../quote";
+
+@Component({
+  selector: "app-add-quote",
+  templateUrl: "./add-quote.component.html",
+  styleUrls: ["./add-quote.component.css"]
+})
+export class AddQuoteComponent implements OnInit {
+  newQuote = new Quote(0, "", "", "", 0, new Date());
+
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote() {
+    this.addQuote.emit(this.newQuote);
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { QuotesComponent } from './quotes/quotes.component';
-import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { QuotesComponent } from "./quotes/quotes.component";
+import { QuotesDetailsComponent } from "./quotes-details/quotes-details.component";
+import { AddQuoteComponent } from "./add-quote/add-quote.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     QuotesComponent,
-    QuotesDetailsComponent
+    QuotesDetailsComponent,
+    AddQuoteComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
