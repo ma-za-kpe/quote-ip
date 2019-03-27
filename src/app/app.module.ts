a0b7419afc9f8e40c2a559c8118c9b9b56ade3db
ma-za-kpe <makpalyy@gmail.com>
2019-03-27 14:20:32 +0200

    add new quotes-details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuotesDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quotes-details/quotes-details.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+
+import { Quote } from "../quote";
+
+@Component({
+  selector: "app-quotes-details",
+  templateUrl: "./quotes-details.component.html",
+  styleUrls: ["./quotes-details.component.css"]
+})
+export class QuotesDetailsComponent implements OnInit {
+  @Input() quote: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  quoteComplete(complete: boolean) {
+    this.isComplete.emit(complete);
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
+}
